5b60d058e5932a1e467ee3a11aa6b65890bef4c8
Iqbalhasanu <108621917+Iqbalhasanu@users.noreply.github.com>
2024-01-05 22:20:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,13 +25,13 @@ include_once("koneksi.php");
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistem Informasi Poliklinik</title>
+    <title>Poliklinik Sumber Sehat</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Sistem Informasi Poliklinik</a>
+            <a class="navbar-brand" href="#">Poliklinik Sumber Sehat</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -135,7 +135,7 @@ include_once("koneksi.php");
             if (isset($_GET['page'])) {
                 include($_GET['page'] . ".php");
             } else {
-                echo "<br><h2>Selamat Datang di Sistem Informasi Poliklinik";
+                echo "<br><h2>Selamat Datang di Poliklinik Sumber Sehat";
 
                 if (isset($_SESSION['username'])) {
                     //jika sudah login tampilkan username
